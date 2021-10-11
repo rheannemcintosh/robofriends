945b4ffd1bada620ea8df7d1ad5404a506e43b3f
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-11 22:20:33 +0100

    Add the initial create-react-app files

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className='f1 tc'>
+                <h1>HELLO RHE</h1>
+                <p>Welcome to React</p>
+            </div>
+        );
+    }
+}
+
+export default Hello;
